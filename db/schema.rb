1bc22bcca7fabf01fc8c2f66fb0e0f02b065e4a9
Este Christophe <este_christophe@hotmail.com>
2024-07-19 13:02:14 +0200

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_19_092556) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_19_093610) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_19_092556) do
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "memos", force: :cascade do |t|
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
